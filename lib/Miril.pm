9ccaeb04da9dab750a653e30e31fb93f887cd825
Peter Shangov <pshangov@yahoo.com>
2009-10-05 18:40:56 +0300

    POD additions

--- lib/Miril.pm
+++ lib/Miril.pm
@@ -9,11 +9,11 @@ Miril - A Static Content Management System
 
 =head1 VERSION
 
-Version 0.003
+Version 0.004
 
 =cut
 
-our $VERSION = '0.003';
+our $VERSION = '0.004';
 $VERSION = eval $VERSION;
 
 use base ("CGI::Application");
