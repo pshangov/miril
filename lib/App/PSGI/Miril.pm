6ab0558c507ba78395a286bd40a2ac466c8b580a
Peter Shangov <pshangov@yahoo.com>
2010-03-08 16:06:31 +0200

    Miril PSGI app gets its own module

---
+++ lib/App/PSGI/Miril.pm
@@ -0,0 +1,18 @@
+package App::PSGI::Miril;
+
+use CGI::Application::PSGI;
+use CGI::Application::Miril;
+
+sub app {
+	return sub {
+		my $env = shift;
+		my $miril = CGI::Application::Miril->new(
+			QUERY => CGI::PSGI->new($env),
+			PARAMS => { cfg_file => 'example/settings/cfg/config.xml' },
+		);
+		$ENV{HTTP_COOKIE} = $env->{HTTP_COOKIE};
+		CGI::Application::PSGI->run($miril);
+	};
+}
+
+1;
