ada3f7937e4ce89d643afc1bbc3c76fe06d5c603
Peter Shangov <pshangov@yahoo.com>
2010-03-25 18:55:05 +0200

    'body' and 'teaser' methods in Miril::Model::File::Post

--- lib/Miril/Model/File/Post.pm
+++ lib/Miril/Model/File/Post.pm
@@ -3,6 +3,12 @@ package Miril::Model::File::Post;
 use strict;
 use warnings;
 
+use autodie;
+use Try::Tiny;
+use Miril::Exception;
+
+use File::Spec;
+
 use base 'Miril::Post';
 
 sub body {
@@ -11,8 +17,36 @@ sub body {
 	if ($self->{body}) {
 		return $self->{body};
 	} else {
-		...
+		$self->_populate;
+		return $self->{body};
 	}
+}
+
+sub teaser {
+	my $self = shift;
+
+	if ($self->{teaser}) {
+		return $self->{teaser};
+	} else {
+		$self->_populate;
+		return $self->{teaser};
+	}
+}
+
+sub _populate {
+	my $self = shift;
+	
+	try {
+		my $post_file = File::Slurp::read_file($self->filename);
+	} catch {
+		##
+	}
+
+	my ($meta, $body) = split( '<!-- END META -->', $post_file, 2);
+	my ($teaser)      = split( '<!-- END TEASER -->', $body, 2);
+
+	$self->{body} = $body;
+	$self->{teaser} = $teaser;
 }
 
 1;
