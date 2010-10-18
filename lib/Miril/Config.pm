f99c9802997f12a2503a40fe359b3294ee7936e9
Peter Shangov <pshangov@yahoo.com>
2010-10-19 00:52:31 +0300

    Started Mousification

--- lib/Miril/Config.pm
+++ lib/Miril/Config.pm
@@ -2,75 +2,199 @@ package Miril::Config;
 
 use strict;
 use warnings;
-use autodie;
 
-use XML::TreePP;
-use Data::AsObject dao => { mode => 'silent' };
-use Ref::List qw(list);
-use File::Spec::Functions qw(catfile catdir);
-use Miril::Topic;
-use Miril::Type;
+use File::Spec ();
 
-sub new {
-	my $class     = shift;
-	my $miril_dir = shift;
-	my $site      = shift;
+use Mouse;
 
-	my $tpp = XML::TreePP->new();
+has 'miril_dir' => 
+(
+	is       => 'ro',
+	required => 1,
+);
 
-	my $base_dir = catdir($miril_dir, 'sites', $site);
-	my $filename = catfile($base_dir, 'cfg', 'config.xml');
-	my $tree = $tpp->parsefile($filename);
-	my $cfg = $tree->{'xml'};
-	
-	### SUPPLY DEFAULT VALUES ###
-	
-	$cfg->{store}          = 'File'           unless defined $cfg->{store};
-	$cfg->{user_manager}   = 'XMLTPP'         unless defined $cfg->{user_manager};
-	$cfg->{filter}         = 'Markdown'       unless defined $cfg->{filter};
-	$cfg->{template}       = 'HTML::Template' unless defined $cfg->{template};
+has 'site' => 
+(
+	is       => 'ro',
+	required => 1,
+);
 
-	$cfg->{posts_per_page} = 10               unless defined $cfg->{posts_per_page};
+has 'site_dir' => 
+(
+	is      => 'ro',
+	builder => sub 
+	{ 
+		my $self = shift;
+		File::Spec->catdir($self->miril_dir, 'sites', $self->site); 
+	},
+);
 
-	$cfg->{cache_data}     = catfile($base_dir, 'cache', 'cache.xml' );
-	$cfg->{latest_data}    = catfile($base_dir, 'cache', 'latest.xml');
-	$cfg->{users_data}     = catfile($base_dir, 'cfg',   'users.xml' );
+has 'store' => 
+(
+	is      => 'ro',
+	default => 'File',
+);
 
-	$cfg->{data_path}      = catdir($base_dir, 'data');
-	$cfg->{tmpl_path}      = catdir($base_dir, 'tmpl');
+has 'user_manager' => 
+(
+	is      => 'ro',
+	default => 'XMLTPP',
+);
 
-	$cfg->{statuses}       = [qw(draft published)];
+has 'filter' => (
+	is      => 'ro',
+	default => 'Markdown',
+);
 
-	$cfg->{sort} = 'published' if !$cfg->{sort} || $cfg->{sort} ne 'modified';
+has 'template' => 
+(
+	is      => 'ro',
+	default => 'HTML::Template',
+);
 
-	my @topics = map { 
-		Miril::Topic->new(
-			id   => $_->{id},
-			name => $_->{name},
-		) 
-	} list $cfg->{topics}{topic};
+has 'posts_per_page' => 
+(
+	is      => 'ro',
+	default => 10,
+);
 
-	my @types = map {
-		Miril::Type->new(
-			id       => $_->{id},
-			name     => $_->{name},
-			location => $_->{location},
-			template => $_->{template},
-		)
-	} list $cfg->{types}{type};
+has 'cache_data' => 
+(
+	is      => 'ro',
+	builder => sub 
+	{ 
+		my $self = shift;
+		File::Spec->catfile($self->site_dir, 'cache', 'cache.xml'); 
+	},
+);
 
-	### SIMPLIFY THE HASHREF ###
-	
-	$cfg->{authors} = $cfg->{authors}{author} ? $cfg->{authors}{author} : [];
-	$cfg->{topics}  = \@topics;
-	$cfg->{types}   = \@types;
-	$cfg->{lists}   = $cfg->{lists}{list};
+has 'latest_data' => 
+(
+	is      => 'ro',
+	builder => sub 
+	{ 
+		my $self = shift;
+		File::Spec->catfile($self->site_dir, 'cache', 'latest.xml'); 
+	},
+);
 
-	### ADD BASE DIR INFO ###
-	
-	$cfg->{base_dir} = $cfg->{domain} . $cfg->{http_dir};
+has 'users_data' => 
+(
+	is      => 'ro',
+	builder => sub 
+	{ 
+		my $self = shift;
+		File::Spec->catfile($self->site_dir, 'cfg', 'users.xml');
+	},
+);
 
-	return dao $cfg;
-}
+has 'data_path' => 
+(
+	is      => 'ro',
+	builder => sub 
+	{ 
+		my $self = shift;
+		File::Spec->catdir($self->site_dir, 'data');
+	},
+);
+
+has 'tmpl_path' => 
+(
+	is      => 'ro',
+	builder => sub 
+	{ 
+		my $self = shift;
+		File::Spec->catdir($self->site_dir, 'tmpl');
+	},
+);
+
+has 'statuses' => 
+(
+	is      => 'ro',
+	isa     => 'ArrayRef',
+	default => sub { [qw(draft published)] },
+);
+
+has 'sort' => 
+(
+	is      => 'ro',
+	default => 'published',
+);
+
+has 'topics' => 
+(
+	is        => 'ro',
+	isa       => 'ArrayRef[Miril::Topic]',
+	predicate => 'has_topics',
+);
+
+has 'types' => 
+(
+	is        => 'ro',
+	isa       => 'ArrayRef[Miril::Type]',
+	predicate => 'has_types',
+);
+
+has 'authors' => 
+(
+	is        => 'ro',
+	isa       => 'ArrayRef',
+	predicate => 'has_authors',
+);
+
+has 'lists' => 
+(
+	is        => 'ro',
+	isa       => 'ArrayRef',
+	predicate => 'has_lists',
+);
+
+has 'base_dir' => 
+(
+	is       => 'ro',
+	required => 1,
+);
+
+has 'output_path' => 
+(
+	is       => 'ro',
+	required => 1,
+);
+
+has 'files_path' => 
+(
+	is      => 'ro',
+	default => sub { File::Spec->catdir( my $_[0]->output_path, 'files' ) },
+);
+
+has 'domain' =>
+(
+	is       => 'ro',
+	required => 1,
+);
+
+has 'http_dir' =>
+(
+	is       => 'ro',
+	required => 1,
+);
+
+has 'files_http_dir' =>
+(
+	is       => 'ro',
+	default => sub { File::Spec->catdir( my $_[0]->http_dir, 'files' ) },
+);
+
+has 'name' =>
+(
+	is      => 'ro',
+	default => 'Miril',
+);
+
+has 'secret' =>
+(
+	is      => 'ro',
+	default => 'Papa was a rolling stone!',
+);
 
 1;
---
+++ lib/Miril/Config/Format/XML.pm
@@ -0,0 +1,71 @@
+package Miril::Config::Format::XML;
+
+use strict;
+use warnings;
+
+use XML::TreePP;
+use Ref::List qw(list);
+use Miril::Topic;
+use Miril::Type;
+
+use Mouse;
+extends 'Miril::Config';
+
+around 'BUILDARGS' => sub
+{
+	my ($orig, $class, $filename) = @_;
+
+	my $tpp = XML::TreePP->new();
+	my $tree = $tpp->parsefile($filename);
+	my $cfg = $tree->{'xml'};
+	
+	my %args;
+
+	my @options = qw(
+		store
+		user_manager
+		filter
+		template
+		posts_per_page
+		sort
+	);
+
+	foreach my $option (@options)
+	{
+		$args{$option} = $cfg->{$option} if defined $cfg->{$option};
+	}
+
+	my @topics = map 
+	{ 
+		Miril::Topic->new(
+			id   => $_->{id},
+			name => $_->{name},
+		) 
+	} list $cfg->{topics}{topic};
+
+	my @types = map 
+	{
+		Miril::Type->new(
+			id       => $_->{id},
+			name     => $_->{name},
+			location => $_->{location},
+			template => $_->{template},
+		)
+	} list $cfg->{types}{type};
+
+	### SIMPLIFY THE HASHREF ###
+	
+	$args{authors} = $cfg->{authors}{author} if $cfg->{authors}{author};
+	$args{lists}   = $cfg->{lists}{list}     if $cfg->{lists}{list};
+	$args{topics}  = \@topics                if @topics;
+	$args{types}   = \@types                 if @types;
+
+	### ADD BASE DIR INFO ###
+	
+	$args{base_dir} = $cfg->{domain} . $cfg->{http_dir};
+
+	return $class->$orig(%args);
+}
+
+1;
+
