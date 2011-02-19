e50a1fd655f6cf414b30e410dbcc2b26a5f0e48d
Peter Shangov <pshangov@yahoo.com>
2011-02-19 19:48:36 +0200

    Refactoring of Miril::Store

--- lib/Miril/Post.pm
+++ lib/Miril/Post.pm
@@ -2,15 +2,16 @@ package Miril::Post;
 
 use strict;
 use warnings;
-
 use Any::Moose;
+use Path::Class;
 
-with 'WWW::Publisher::Static::Post';
+### ID ###
 
 has 'id' => 
 (
-	qw(:ro :required),
+	is            => 'ro',
 	isa           => 'Str',
+	required      => 1,
 	documentation => 'Unique text ID of the post',
 );
 
@@ -75,7 +76,7 @@ has 'status' =>
 (
 	qw(:rw :required),
 	isa           => 'Str',
-	default       => 'draft'
+	builder       => '_build_status'
 	documentation => 'Post status: draft or published',
 );
 
@@ -120,12 +121,99 @@ has 'tag_url' =>
 	documentation => 'Tag URL for this post, to be used e.g. in Atom feeds',
 );	
 
-### LAZY LOADING OF SOURCE, BODY AND TEASER ###
+### CONSTRUCTORS ###
+
+sub new_from_id
+{
+	my ($class, $cfg, $id) = @_;
+
+	# get contents of source file
+	my $source_path = file(_inflate_source_path_from_id($id));
+
+	# split sourcefile into sections
+	my ($body, $teaser, $source, $meta) = _parse_source_file($source_path);
+
+	# parse metadata
+	my %meta = _parse_meta($meta);
+
+	# expand metadata into objects
+	my $author = _inflate_object_from_id('author', $cfg, $meta{type});
+	my $topics = _inflate_object_from_id('topics', $cfg, $meta{type});
+	my $type = _inflate_object_from_id('type', $cfg, $meta{type});
+	
+	# prepare the remaining attributes
+	my $title = $meta{title};
+	my $published = $meta{'published'} ? Miril::DateTime->new(iso2time($meta{'published'})) : undef;
+	my $url = $meta{'published'} ? _inflate_url($id, $type, $published) : undef,
+	my $path = _inflate_path_from_id($id, $type),
+	
+	return $class->new(
+		id          => $id,
+		title       => $title,
+		author      => $author
+		topics      => $topics,
+		type        => $type,
+		body        => $body,
+		teaser      => $teaser,
+		source      => $source,
+		path        => $path,
+		source_path => $filename,
+		url         => $url,
+		published   => $published,
+	);
+}
+
+sub new_from_cache
+{
+	my ($class, $cfg, %cache) = @_;
+
+	my $author = _inflate_object_from_id('author', $cfg, $cache{type});
+	my $topics = _inflate_object_from_id('topics', $cfg, $cache{type});
+	my $type = _inflate_object_from_id('type', $cfg, $cache{type});
+
+	my $published = $cache{'published'} ? Miril::DateTime->new(iso2time($meta{'published'})) : undef;
+	my $url = $cache{'published'} ? _inflate_url($id, $type, $published) : undef,
+
+	return $class->new(
+		id          => $cache{id},
+		title       => $cache{title},
+		author      => $author
+		topics      => $topics,
+		type        => $type,
+		path        => file($cache{path}),
+		source_path => file($cache{source_path}),
+		url         => $url,
+		published   => $published,
+	);
+}
+
+sub new_from_params
+{
+	my ($class, $cfg, %params) = @_;
+
+	my $author = _inflate_object_from_id('author', $cfg, $params{type});
+	my $topics = _inflate_object_from_id('topics', $cfg, $params{type});
+	my $type = _inflate_object_from_id('type', $cfg, $params{type});
+
+	my $published = _inflate_date_published($params{published}, $params{status});
+
+	return $class ->new(
+		id        => $params{id},
+		title     => $params{title},
+		author    => $author,
+		topics    => $topics,
+		type      => $type,
+		source    => $params{source},
+		published => $published,
+	);
+}
+
+### BUILDERS ###
 
 sub _build_body
 {
 	my $self = shift;
-	my ($source, $body, $teaser) = $self->_populate;
+	my ($source, $body, $teaser) = $self->_parse_source_file($self->source_path);
 	$self->source($source);
 	$self->teaser($teaser);
 	return $body;
@@ -134,7 +222,7 @@ sub _build_body
 sub _build_source
 {
 	my $self = shift;
-	my ($source, $body, $teaser) = $self->_populate;
+	my ($source, $body, $teaser) = $self->_parse_source_file($self->source_path);
 	$self->body($body);
 	$self->teaser($teaser);
 	return $source;
@@ -143,24 +231,22 @@ sub _build_source
 sub _build_teaser
 {
 	my $self = shift;
-	my ($source, $body, $teaser) = $self->_populate;
+	my ($source, $body, $teaser) = _parse_source_file($self->source_path);
 	$self->source($source);
 	$self->body($body);
 	return $teaser;
 }
 
-sub _populate 
-{
-	my $self = shift;
+### PRIVATE UTILITY FUNCTIONS ###
 
-	my $post_file = try {
-		File::Slurp::read_file($self->path);
-	} catch {
-		Miril::Exception->throw(
-			message => "Cannot load data file",
-			errorvar => $_,
-		);
-	};
+sub _parse_source_file 
+{
+	my $source_path = shift;
+
+	my $post_file = $source_path->slurp or Miril::Exception->throw(
+		message => "Cannot load data file",
+		errorvar => $_,
+	);
 
 	my ($meta, $source) = split( /\n\n/, $post_file, 2);
 	my ($teaser) = split( '<!-- BREAK -->', $source, 2);
@@ -168,7 +254,34 @@ sub _populate
 	# temporary until we introduce multiple filters
 	my $filter = Miril::Filter::Markdown->new;
 
-	return $filter->to_xhtml($source), $filter->to_xhtml($teaser), $source;
+	return $filter->to_xhtml($source), $filter->to_xhtml($teaser), $source, $meta;
 }
+
+sub _parse_meta 
+{
+	my ($meta) = @_;
+
+	my @lines = split /\n/, $meta;
+	my %meta;
+	
+	foreach my $line (@lines) {
+		if ($line =~ /^(Published|Title|Type|Author|Status):\s+(.+)/) {
+			my $name = lc $1;
+			my $value = $2;
+			$value  =~ s/\s+$//;
+			$meta{$name} = $value;
+		} elsif ($line =~ /Topics:\s+(.+)/) {
+			my $value = lc $1;
+			$value  =~ s/\s+$//;
+			my @values = split /\s+/, $value;
+			$meta{topics} = \@values;
+		}
+	}
+	
+	$meta{topics} = [] unless defined $meta{topics};
+
+	return %meta;
+}
+
 
 1;
--- lib/Miril/Store.pm
+++ lib/Miril/Store.pm
@@ -3,11 +3,25 @@ package Miril::Store;
 use strict;
 use warnings;
 
-use File::Slurp;
-use Syntax::Keyword::Gather qw(gather take);
+use Syntax::Keyword::Gather;
 
 use Mouse;
 with 'WWW::Publisher::Static::Store';
+
+has 'posts' =>
+(
+	is       => 'rw',
+	isa      => 'HashRef[Miril::Post]',
+	builder  => '_build_posts',
+	lazy     => 1,
+	traits   => ['Hash'],
+	handles  => 
+	{
+		get_post_by_id => 'get',
+		get_posts      => 'values',
+		add_post       => 'set',
+	},
+);
 
 has 'cfg' =>
 (
@@ -16,163 +30,52 @@ has 'cfg' =>
 	required => 1,
 );
 
-has 'util' =>
+has 'cache' =>
 (
-	is       => 'ro',
-	isa      => 'Miril::Util',
-	required => 1,
+	is      => 'ro',
+	isa     => 'Miril::Cache',
+	lazy    => 1,
+	builder => '_build_cache',
 );
 
-has 'filter' =>
-(
-	is       => 'ro',
-	isa      => 'Miril::Filter',
-	required => 1,
-);
+### BUILDERS ###
 
-has 'tpp' =>
-(
-	is       => 'rw',
-	isa      => 'XML::TreePP',
-);
+sub _build_cache
+{
+	my $self = shift;
+	return Miril::Cache->new($self->cfg->cache_data)
+}
 
-has 'tree' =>
-(
-	is       => 'rw',
-	isa      => 'HashRef',
-);
-
+sub _build_posts 
+{
+	my $self = shift;
+	return [ map { Miril::Post->new_from_cache($self->cfg, %$_) } $self->cache->posts ];
+}
 
 ### PUBLIC METHODS ###
 
-sub get_post_by_id
-{
-	my ($self, $id) = @_;
-
-	my $cfg = $self->cfg;
-	my $util = $self->util;
-
-	#TODO _inflate_source_path
-	my $filename = $util->inflate_in_path($id);
-	
-	my $post_file = File::Slurp::read_file($filename) or
-		Miril::Exception->throw(
-			message => "Could not read data file", 
-			errorvar => $_,
-		);
-
-	#TODO _post_parse_sections
-	my ($meta, $source) = split( /\n\n/, $post_file, 2);
-	my ($teaser)        = split( '<!-- BREAK -->', $source, 2);
-    
-	#TODO _post_parse_meta
-	my %meta = _parse_meta($meta);
-
-	#TODO _build_date_modified
-	my $modified = $util->inflate_date_modified($filename);
-	#TODO _inflate_type_from_id
-	my $type = $util->inflate_type($meta{'type'});
-	
-	my $published = $meta{'published'} ? Miril::DateTime->new(iso2time($meta{'published'})) : undef;
-
-	Miril::Post->new_with_cfg(
-		id => $id,
-		source => $source, # optional
-		meta => $meta,
-		cfg => $cfg,
-	);
-
-	# OR:
-	
-	return Miril::Store::File::Post->new(
-		id        => $id,
-		title     => $meta{'title'},
-		body      => $self->filter->to_xhtml($source),
-		teaser    => $self->filter->to_xhtml($teaser),
-		source    => $source,
-		path      => $util->inflate_out_path($id, $type),
-		source_path => $filename,
-		#modified  => Miril::DateTime->new($modified),
-		published => $published,
-		type      => $type,
-		#TODO _inflate_url
-		url       => $published ? $util->inflate_post_url($id, $type, $published) : undef,
-		author    => $util->inflate_author($meta{'author'}),
-		topics    => $util->inflate_topics( list $meta{'topics'} ),
-	);
-}
-
-sub get_posts 
+sub search
 {
 	my ($self, %params) = @_;
 
-	my $cfg = $self->cfg;
-	my $util = $self->util;
+	return $self->get_posts unless %params;
 
-	my $cache = Miril::Cache->new($cfg->cache_data);
-
-	if ($cache->is_full)
+	# search
+	my @posts = gather 
 	{
-		my @posts = map { ... } $cache->posts;
-	}  
-
-	my @post_ids;
-
-	#TODO $cache->check_state;
-	# for each post, check if the data in the cache is older than the data in the filesystem
-	foreach my $post (@posts) {
-		if ( -e $post->source_path ) {
-			push @post_ids, $post->id;
-			
-			my $modified = $util->inflate_date_modified($post->in_path);
-			if ( $modified > $post->modified->epoch ) {
-				$post = $self->get_post($post->id);
-				$dirty++;
-			}
-		} else {
-			undef $post;
-			$dirty++;
-		}
-	}
-
-	# clean up posts deleted from the cache
-	@posts = grep { defined } @posts;
-	
-	#TODO my $post = $self->get_post($_); push @posts, $cache->add_items($post) for $cache->posts_needing_update;
-	# check for entries missing from the cache
-	opendir(my $data_dir, $cfg->data_path);
-
-	while ( my $id = readdir($data_dir) ) {
-		next if -d $id;
-		unless ( first {$_ eq $id} @post_ids ) {
-			my $post = $self->get_post($id);
-			push @posts, $post;
-			$dirty++;
-		}
-	}
-	
-
-	# update cache file
-	$self->cache->update;
-
-	# filter posts
-	if (%params)
-	{
-		@posts = gather 
+		foreach my $post ($self->get_posts)
 		{
-			for my $cur_post (@posts)
-			{
-				my $title_rx = $params{'title'};
-				next if $params{'title'}  && $cur_post->title    !~ /$title_rx/i;
-				next if $params{'author'} && $cur_post->author   ne $params{'author'};
-				next if $params{'type'}   && $cur_post->type->id ne $params{'type'};
-				next if $params{'status'} && $cur_post->status   ne $params{'status'};
-				next if $params{'topic'}  && !first {$_->id eq $params{'topic'}} list $cur_post->topics;
-				take $cur_post;
-			}
-		};
-	} 
+			my $title_rx = $params{'title'};
+			next if $params{'title'}  && $post->title    !~ /$title_rx/i;
+			next if $params{'author'} && $post->author   ne $params{'author'};
+			next if $params{'type'}   && $post->type->id ne $params{'type'};
+			next if $params{'status'} && $post->status   ne $params{'status'};
+			next if $params{'topic'}  && !first {$_->id eq $params{'topic'}} $post->get_topics;
+			take $post;
+		}
+	};
 
+	# sort
 	if ($cfg->sort eq 'modified')
 	{
 		@posts = sort { $b->modified->epoch <=> $a->modified->epoch } @posts;
@@ -189,6 +92,7 @@ sub get_posts
 		}
 	}
 	
+	# limit
 	if ($params{'last'})
 	{
 		my $count = ( $params{'last'} < @posts ? $params{'last'} : @posts );
@@ -200,120 +104,40 @@ sub get_posts
 
 sub save 
 {
-	my ($self, %post) = @_;
+	my ($self, %params) = @_;
 
-	my $post = dao \%post;
-	my $cfg = $self->cfg;
-	my $util = $self->util;
-	
-	my @posts = $self->get_posts;
-	
-	if ($post->old_id) {
-		# this is an update
+	my $post = Miril::Post->new_from_params($self->cfg, %params);
+	$self->add_post($post->id => $post);
 
-		for (@posts) {
-			if ($_->id eq $post->old_id) {
-				$_->{id}        = $post->id;
-				$_->{author}    = $post->author;
-				$_->{title}     = $post->title;
-				$_->{type}      = $util->inflate_type($post->type);
-				$_->{topics}    = $util->inflate_topics(list $post->topics);
-				$_->{status}    = $post->status;
-				$_->{source}    = $post->source;
-				if ($post->status eq 'published')
-				{
-					$_->{published} = $util->inflate_date_published($_->published, $post->status);
-				}
-				last;
-			}
-		}
-		
-		# delete the old file if we have changed the id
-		if ($post->old_id ne $post->id) {
-			try {
-				unlink($cfg->data_path . '/' . $post->old_id);
-			} catch {
-				Miril::Exception->throw( 
-					message => "Cannot delete old version of renamed post",
-					errorvar => $_
-				);
-			};
-		}	
-
-	} else {
-		# this is a new post
-		my $new_post = Miril::Store::File::Post->new(
-			id        => $post->id,
-			author    => ($post->author or undef),
-			title     => $post->title,
-			type      => $util->inflate_type($post->type),
-			topics    => $util->inflate_topics($post->topics),
-			published => $util->inflate_date_published(undef, $post->status),
-			status    => $post->status,
-			source    => $post->source,
-		);
-		push @posts, $new_post;
-	}
-
-	# update the cache file
-	my $new_tree;
-	$new_tree->{xml}{post} = _generate_cache_hash(@posts);
-	$self->{tree} = $new_tree;
-
-	try
+	# delete the old file if we have changed the id
+	if ($params{old_id} and ($params{old_id} ne $params{id}))
 	{
-		$self->tpp->writefile($cfg->cache_data, $new_tree)
-	}
-	catch
-	{
-		Miril::Exception->throw(
-			message => "Could noe update cache file", 
-			erorvar => $_,
-		);
-	};
-	
+		$self->delete($params{old_id});
+	}	
+
 	# update the data file
-	my $content;
 	
-	$post = first { $_->id eq $post->id } @posts;
+	my $content = _generate_content($post);
 
-	$content .= "Title: " . $post->title . "\n";
-	$content .= "Author: " . $post->author . "\n" if $post->author;
-	$content .= "Type: " . $post->type->id . "\n";
-	$content .= "Published: " . $post->published->iso . "\n" if $post->published;
-	$content .= "Topics: " . join(" ", map { $_->id } list $post->topics) . "\n\n";
-	$content .= $post->source;
-
-	try
-	{
-		my $fh = IO::File->new( catfile($cfg->data_path, $post->id), "w") or die $!;
-		$fh->print($content);
-		$fh->close;
-	}
-	catch
-	{
-		Miril::Exception->throw(
-			message => "Could not update data file", 
-			erorvar => $_,
-		);
-	};
+	my $fh = $post->source_path->open('>') or Miril::Exception->throw(
+		message => "Could not open data file for writing",
+		erorvar => $_,
+	);
+	$fh->print($content);
+	$fh->close;
 }
 
 sub delete
 {
 	my ($self, $id) = @_;
 
-	try
-	{
-		unlink catfile($self->cfg->data_path, $id);
-	}
-	catch
-	{
-		Miril::Exception->throw(
-			message => "Could not delete data file", 
-			erorvar => $_,
-		);
-	};
+	my $post = $self->get_post_by_id($id);
+	$post->remove or Miril::Exception->throw( 
+		message => "Cannot delete post",
+		errorvar => $_
+	);
+	
+	$self->posts->delete($id);
 }
 
 sub get_latest 
@@ -341,7 +165,6 @@ sub get_latest
 			errorvar => $_,
 		);
 	};
-	
 
 	return \@posts;
 }
@@ -392,67 +215,20 @@ sub add_to_latest
 
 ### PRIVATE FUNCTIONS ###
 
-sub _parse_meta 
+sub _generate_content
 {
-	my ($meta) = @_;
+	my $post = shift;
+	my $content;
 
-	my @lines = split /\n/, $meta;
-	my %meta;
-	
-	foreach my $line (@lines) {
-		if ($line =~ /^(Published|Title|Type|Author|Status):\s+(.+)/) {
-			my $name = lc $1;
-			my $value = $2;
-			$value  =~ s/\s+$//;
-			$meta{$name} = $value;
-		} elsif ($line =~ /Topics:\s+(.+)/) {
-			my $value = lc $1;
-			$value  =~ s/\s+$//;
-			my @values = split /\s+/, $value;
-			$meta{topics} = \@values;
-		}
-	}
-	
-	$meta{topics} = [] unless defined $meta{topics};
+	$content .= "Title: "     . $post->title          . "\n";
+	$content .= "Type: "      . $post->type->id       . "\n";
+	$content .= "Author: "    . $post->author         . "\n" if $post->author;
+	$content .= "Published: " . $post->published->iso . "\n" if $post->published;
+	$content .= "Topics: "    . join( " ", map { $_->id } $post->get_topics ) . "\n\n";
 
-	return %meta;
-}
+	$content .= $post->source;
 
-
-sub search {
-if ( $list->match->id )
-		{
-			push @posts, $miril->store->get_post($_) for $list->match->id->list;
-		}
-		else 	
-		{
-			my @params = qw(
-				author
-				type
-				status
-				topic
-				created_before
-				created_on
-				created_after
-				updated_before
-				updated_on
-				updated_after
-				published_before
-				published_on
-				published_after
-				last
-			);
-		
-			my %params;
-
-			foreach my $param (@params) {
-				if ( exists $list->match->{$param} ) {
-					$params{$param} = $list->match->{$param};
-				}
-			}
-
-			@posts = $miril->store->get_posts(%params);
-		}
+	return $content;
 }
 
 1;
