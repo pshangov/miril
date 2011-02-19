b8c16f3fdf7a5a77df4aa8229f4a65a5bc93cb2c
Peter Shangov <pshangov@yahoo.com>
2011-02-19 21:47:33 +0200

    Started refactoring Miril::DateTime

--- lib/Miril/DateTime.pm
+++ lib/Miril/DateTime.pm
@@ -2,20 +2,137 @@ package Miril::DateTime;
 
 use strict;
 use warnings;
-use autodie;
 
-use overload '""' => \&epoch;
+use Mouse;
+use Try::Catch;
+use Time::Local  qw();
+use Date::Format qw();
+use Carp::Croak  qw();
 
-use POSIX;
-use Time::Local qw(timelocal);
-use Miril::DateTime::ISO::Simple qw(time2iso);
+use overload '0+' => \&as_epoch;
 
-sub new      { bless \$_[1], $_[0] }
-sub epoch    { ${$_[0]} }
-sub iso      { time2iso(${$_[0]}) }
+has 'year'     => ();
+has 'month'    => ();
+has 'day'      => ();
+has 'hour'     => ();
+has 'minute'   => ();
+has 'second'   => ();
+has 'timezone' => ();
 
-no warnings qw(redefine);
-sub strftime { POSIX::strftime($_[1], localtime(${$_[0]})) };
-use warnings qw(redefine);
+### CONSTRUCTORS ###
+
+sub now
+{
+	my $class = shift;
+
+	my @t = localtime time;
+	
+	return $class->new(
+		year   => $t[5] + 1900,
+		month  => $t[4] + 1,
+		day    => $t[3],
+		hour   => $t[2],
+		minute => $t[1],
+		second => $t[0],
+	);
+}
+
+sub from_string
+{
+	my ($class, $string, $format_type);
+
+	if ($format_type eq 'default')
+	{
+		return $class->from_ymdhm($string);
+	}
+	elsif ($format_type eq 'iso')
+	{
+		return $class->from_iso($string);
+	}
+	else
+	{
+		Carp::croak("Ivalid format type $format_type supplied to from_string");
+	}
+}
+
+sub from_ymdhm
+{
+	my ($class, $string) = @_;
+
+	unless ( $string =~ /^(\d\d\d\d)-(\d\d)-(\d\d)\s(\d\d):(\d\d)$/ ) 
+	{
+		Carp::croak("Invalid time format (does not match 'YYYY:MM:DD HH:MM')");
+	}
+	
+	return $class->new(
+		year   => $1 + 0,
+		month  => $2 + 0,
+		day    => $3 + 0,
+		hour   => $4 + 0,
+		minute => $5 + 0,
+		second => 0,
+	);
+}
+
+sub from_iso
+{
+	my ($class, $iso) = @_;
+
+	# 2009-11-26T16:55:34+02:00
+	my $re = qr/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})([+-])(\d{2}):(\d{2})/;
+
+	if ( $iso =~ $re ) {
+		my $year = $1;
+		my $month = $2 - 1;
+		my $day = $3;
+		my $hour = $4;
+		my $min = $5;
+		my $sec = $6;
+		my $sign = $7;
+		my $offset = $8*60*60 + $9*60;
+		$offset = -$offset if $sign eq '-';
+
+		my $local = time;
+		my $gm = Time::Local::timelocal( gmtime $local );
+		my $local_offset = $local - $gm;
+		
+		my $time = Time::Local::timelocal($sec, $min, $hour, $day, $month, $year);
+
+		if ( $offset == $local_offset ) {
+			return $time;
+		} else {
+			my $abs = abs($local_offset - $offset);
+			$local_offset > $offset ? return $time + $abs : return $time - $abs;
+		}
+	}
+}
+
+sub from_epoch
+{
+
+}
+
+### CONVERSION ###
+
+sub as_iso 
+{
+	my $time = shift;
+
+	# get timezone
+	my $local = time;
+	my $gm = Time::Local::timelocal( gmtime $local );
+	my $sign = qw( + + - )[ $local <=> $gm ];
+	my $tz = sprintf "%s%02d:%02d", $sign, (gmtime abs( $local - $gm ))[2,1];	
+
+	# iso
+	my @time = localtime $time;
+	my $iso = strftime("%Y-%m-%dT%H:%M:%S$tz", @time);
+
+	return $iso;	
+}
+
+sub as_datetime {}
+sub as_strftime {}
+sub as_epoch {}
 
 1;
